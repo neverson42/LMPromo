fac7e7aa022e1435871299aa80d8a70afc621c98
neverson42 <35622011+neverson42@users.noreply.github.com>
2021-02-21 10:35:17 -0600

    Add files via upload
    V 2.0 - changed to better source for codes.

--- LMPromo.js
+++ LMPromo.js
@@ -1,14 +1,13 @@
-/*
-MAJOR thank you to Nekiplay for hosting the active codes on his server. Check out his Lords Mobile API!  https://github.com/Nekiplay
-*/
-
 //require request
 const request = require('request');
+//require file strucure
+const fs = require('fs');
+//require jsdom
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 //url to check for codes
-const url = "https://lordsmobile.fandom.com/wiki/Promotion_Codes";
-//needpermission from git owner still
-const gitURL = "https://raw.githubusercontent.com/Nekiplay/LordsMobileAPI/master/ServerSide/WorkingPromoCodes.txt"
+const url = "https://www.mejoress.com/en/lords-mobile-codes/";
 
 //require the discord.js module
 const Discord = require('discord.js');
@@ -19,26 +18,32 @@ const client = new Discord.Client();
 //import login token
 const auth = require('./auth.json');
 
+function scrape_codes(data) {
+	var doc = new JSDOM(data);
+	var scraped_codes = doc.window.document.getElementsByTagName("ul")[6].getElementsByTagName("strong");
+	var edit_date = doc.window.document.getElementsByClassName("post-modified-info")[0].innerHTML;
+	return scraped_codes[0].innerHTML+"\n"+scraped_codes[1].innerHTML+"\n"+scraped_codes[2].innerHTML+"\n"+edit_date;
+}
+
 // this event will only trigger one time after logging in
 // when the client is ready, run this code
 client.once('ready', () => {
+	client.user.setActivity("Listening for !promo");
 	console.log('Ready!');
 });
 
-function loadData() {
-	return jQuery.get(gitURL);
-}
-
 client.on('message', message => {
-		if(message.content.toLowerCase() == "!promo") {
-			request(gitURL, function (error, response, codes) {
-			console.log(response && response.statusCode); // Print the response status code if a response was received
+	if(message.content.toLowerCase() == "!promo") {
+		request(url, function (error, response, pg_source) {
 			if(error || (response && response.statusCode) != "200")
 				message.channel.send("Error reading codes. Please message #neverson42#1672 so I can fix it! Thanks.")
-			else
+			else {
+				codes = scrape_codes(pg_source);
+				console.log(codes);
 				message.channel.send("Active codes:\n"+codes);
-			});
-		}
+			}
+		});
+	}
 });
 
 // login to Discord with your app's token
